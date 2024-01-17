e4cb7fa6586192dd9ebe681b2102db938b1f04a0
Crislaine <clpaula3007@gmail.com>
2024-01-17 15:15:15 -0300

    Adicionar arquivos de projeto.

---
+++ ImportarImagemProjetoCanal/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ImportarImagemProjetoCanal.Models;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ImportarImagemProjetoCanal.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options ) : base(options)
+        {
+        }
+
+        public DbSet<ImagemModel> Imagens { get; set; }
+    }
+}
